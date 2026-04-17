9564c50074d6ca986ec5756c0c0f53ac4f69a7a2
bombiii <zsomborvarga077@gmail.com>
2026-04-17 13:49:38 +0200

    Added constants to parking calculator

--- parking_calculator/main.py
+++ parking_calculator/main.py
@@ -1,4 +1,13 @@
 from pathlib import Path
+import math
+from datetime import datetime
+
+FREE_MINUTES = 30
+LOW_RATE = 300
+LOW_RATE_HOURS = 3
+HIGH_RATE = 500
+DAY_CAP = 10_000
+DAY_MINUTES = 24 * 60
 
 
 def main():
