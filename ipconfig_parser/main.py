fa1eb6d997eea520cc6659962fa6630f46f9d6bb
bombiii <zsomborvarga077@gmail.com>
2026-04-18 17:46:25 +0200

    Update file extension filter in main function to process .txt files and read content

--- ipconfig_parser/main.py
+++ ipconfig_parser/main.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
 
-def main():
-    for path in sorted(Path(".").glob("*.log")):
-        print(path.name)
 
+def main():
+    for path in sorted(Path(".").glob("*.txt")):
+        print(path.name)
+        text = Path(path).read_text(encoding="utf-16")
+        print(text)
 
 if __name__ == "__main__":
     main()
